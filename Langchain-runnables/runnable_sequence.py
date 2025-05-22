2a9b185d6d3e165b55a887c2eea28bebc4fb42a6
PRIYARANJAN PAUL <107647288+PriyaranjanPaul11@users.noreply.github.com>
2025-05-22 12:57:17 +0530

    Add files via upload

---
+++ Langchain-runnables/runnable_sequence.py
@@ -0,0 +1,31 @@
+from langchain_community.llms import HuggingFaceHub
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+from langchain.schema.runnable import RunnableSequence
+
+load_dotenv()
+
+prompt1 = PromptTemplate(
+    template='Write a joke about {topic}',
+    input_variables=['topic']
+)
+
+llm = HuggingFaceEndpoint(
+    repo_id="deepseek-ai/DeepSeek-Prover-V2-671B",
+    task="text-generation"
+)
+
+model = ChatHuggingFace(llm=llm)
+
+parser = StrOutputParser()
+
+prompt2 = PromptTemplate(
+    template='Explain the following joke - {text}',
+    input_variables=['text']
+)
+
+chain = RunnableSequence(prompt1, model, parser, prompt2, model, parser)
+
+print(chain.invoke({'topic':'AI'}))
